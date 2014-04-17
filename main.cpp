7be8aa02baeac93878a734d73860d8188f7fe029
Steven Lovegrove <stevenlovegrove@gmail.com>
2014-04-17 16:15:23 +0100

    Support multiple textures as input.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ const char* shadertoy_header =
         "uniform float     iChannelTime[4];       // channel playback time (in seconds)\n"
         "uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)\n"
         "uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click\n"
-        "uniform sampler2D iChannel0;             // input channel. XX = 2D/Cube\n"
         "uniform vec4      iDate;                 // (year, month, day, time in s)\n";
 
 struct ShaderToyHandler : public pangolin::Handler
@@ -57,29 +56,36 @@ int main( int argc, char** argv )
     ShaderToyHandler handler;
     pangolin::DisplayBase().SetHandler(&handler);
 
-    // Load GlSl from file, adding header
-    std::ifstream glsl_file(argv[1]);
+    // GLSL Source, starting with header
     std::stringstream glsl_buffer;
     glsl_buffer << shadertoy_header;
-    glsl_buffer << glsl_file.rdbuf();
     
+    // Load input textures
+    pangolin::GlTexture iChannel[4];
+    GLfloat sq_vert[] = { -1,-1,  1,-1,  1, 1,  -1, 1 };
+    for(int i=0; i<4; ++i) {
+        if(argc > 2+i) {
+            pangolin::TypedImage img = pangolin::LoadImage(argv[2+i]);
+            GLint channels;
+            GLenum format;
+            SetGlFormat(channels,format,img.fmt);
+            iChannel[i].Reinitialise(img.w, img.h, channels, true, 0, channels, format, img.ptr);
+            img.Dealloc();
+
+            glActiveTexture(GL_TEXTURE0+i);
+            iChannel[i].Bind();
+            glsl_buffer << "uniform sampler2D iChannel" << i << ";" << std::endl;
+        }
+    }
+
+    // Load ShaderToy body from file
+    std::ifstream glsl_file(argv[1]);
+    glsl_buffer << glsl_file.rdbuf();
+
     // Compile / link shader
     pangolin::GlSlProgram prog;
     prog.AddShader( pangolin::GlSlFragmentShader, glsl_buffer.str() );
     prog.Link();
-
-    pangolin::GlTexture iChannel0;
-    if(argc > 2) {
-        pangolin::TypedImage img = pangolin::LoadImage(argv[2]);
-        GLint channels;
-        GLenum format;
-        SetGlFormat(channels,format,img.fmt);
-        iChannel0.Reinitialise(img.w, img.h, channels, true,0,channels,format,img.ptr);
-        img.Dealloc();
-    }
-
-    GLfloat sq_vert[] = { -1,-1,  1,-1,  1, 1,  -1, 1 };
-    GLfloat sq_tex[]  = { 0,0,  1,0,  1,1,  0,1  };
 
     float iGlobalTime = 0;
     while( !pangolin::ShouldQuit() )
@@ -92,14 +98,10 @@ int main( int argc, char** argv )
         prog.SetUniform("iGlobalTime", iGlobalTime );
         prog.SetUniform("iMouse", handler.x, handler.y, 0.0f, 0.0f);
 
-        iChannel0.Bind();
         glEnableClientState(GL_VERTEX_ARRAY);
-        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
         glVertexPointer(2, GL_FLOAT, 0, sq_vert);
-        glTexCoordPointer(2, GL_FLOAT, 0, sq_tex);
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
         glDisableClientState(GL_VERTEX_ARRAY);
-        glDisableClientState(GL_TEXTURE_COORD_ARRAY);
         prog.Unbind();
         
         pangolin::FinishFrame();
